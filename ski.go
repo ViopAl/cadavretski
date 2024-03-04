8d198edd77b97681a4547e528c3afee10e985771
colaub <47823470+colaub@users.noreply.github.com>
2024-03-04 16:12:05 +0100

    Update ski.go
    create a BaseSupply struct to check what we put inside the freezer

--- ski.go
+++ ski.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+type BaseSupply struct {
+	name string
+}
+
 type Freezer struct {
 	latitude, longitude float64
 }
 type Beer struct {
+	BaseSupply
 	alcohol_percent_min, alcohol_percent_max float64
 }
 
